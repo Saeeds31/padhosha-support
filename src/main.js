2963082919555074121b1a463e43eb962c2d38af
saeed dehghan <saeeddehghans31@gmail.com>
2026-05-14 11:19:18 +0330

    ;

--- src/main.js
+++ src/main.js
@@ -19,8 +19,8 @@ import Vue3PersianDatetimePicker from "vue3-persian-datetime-picker";
 import "@boindil/vue-file-agent-next/dist/vue-file-agent-next.css";
 // api.padhosha.ir
 // 127.0.0.1:8000
-axios.defaults.baseURL = "http://api.padhosha.ir/api/v1/employer";
-window.baseImageAddress = "http://api.padhosha.ir/storage_public/";
+axios.defaults.baseURL = "https://api.padhosha.ir/api/v1/employer";
+window.baseImageAddress = "https://api.padhosha.ir/storage_public/";
 const app = createApp(App);
 // app.use(CKEditor)
 app.use(Vue3Toastify, {
